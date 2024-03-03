43546e39fba48f251e06f6b7dc53ac38d0659d29
lthero-big <ltherowlh@gmail.com>
2024-03-03 11:05:45 +0800

    first

---
+++ exact.py
@@ -0,0 +1,126 @@
+import torch
+from inverse_stable_diffusion_gs import InversableStableDiffusionPipeline
+import argparse
+from PIL import Image
+from tqdm import tqdm
+from statistics import mean, stdev
+from sklearn import metrics
+from scipy.stats import norm
+from diffusers import DPMSolverMultistepScheduler
+import open_clip
+from optim_utils import *
+from io_utils import *
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+import numpy as np
+from scipy.stats import norm
+
+def exactract_latents(args):
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    scheduler = DPMSolverMultistepScheduler.from_pretrained(args.model_id, subfolder='scheduler')
+    pipe = InversableStableDiffusionPipeline.from_pretrained(
+        args.model_id,
+        scheduler=scheduler,
+        torch_dtype=torch.float16,
+        revision='fp16',
+        )
+    pipe = pipe.to(device)
+    tester_prompt = ''
+    # assume at the detection time, the original prompt is unknown，原论文中使用的就是空prompt
+    text_embeddings = pipe.get_text_embedding(tester_prompt)
+    # 图像路径
+    orig_image_path = args.orig_image_path
+    # 格式PIL.Image.Image
+    orig_image = Image.open(orig_image_path)
+    # 
+    img_w = transform_img(orig_image).unsqueeze(0).to(torch.float16).to(device)
+    # 
+    image_latents = pipe.get_image_latents(img_w, sample=False)
+    # 获得逆向后的噪声矩阵，这里与推理步数有关，越大会变好
+    reversed_latents = pipe.forward_diffusion(
+            latents=image_latents,
+            text_embeddings=text_embeddings,
+            guidance_scale=1,
+            num_inference_steps=args.num_inference_steps,
+        )
+
+    return reversed_latents.cpu()
+
+
+
+def recover_exactracted_message(reversed_latents, key, nonce, l=1):
+    # 初始化Cipher
+    cipher = Cipher(algorithms.ChaCha20(key, nonce), mode=None, backend=default_backend())
+    decryptor = cipher.decryptor()
+    
+    # 从reversed_latents中恢复m的二进制表示
+    reconstructed_m_bits = []
+    for z_s_T_value in np.nditer(reversed_latents):
+        # 使用norm.ppf的逆操作恢复原始的y值
+        y_reconstructed = norm.cdf(z_s_T_value) * 2**l
+        reconstructed_m_bits.append(int(y_reconstructed))
+
+    # 将二进制位转换为字节
+    m_reconstructed_bytes = bytes(int(''.join(str(bit) for bit in reconstructed_m_bits[i:i+8]), 2) for i in range(0, len(reconstructed_m_bits), 8))
+
+    # 解密m以恢复扩散过程前的数据s_d
+    s_d_reconstructed = decryptor.update(m_reconstructed_bytes) + decryptor.finalize()
+
+    # 假设原始水印消息k是s_d的前32字节
+    k_reconstructed = s_d_reconstructed[:32]
+
+    # 将重构的k转换为十六进制表示
+    k_reconstructed_hex = k_reconstructed.hex()
+    
+    # 将重构的k转换为二进制表示
+    k_reconstructed_bin = ''.join(format(byte, '08b') for byte in k_reconstructed)
+    
+    return k_reconstructed_bin
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='exactract watermark')
+    parser.add_argument('--model_id', default='stabilityai/stable-diffusion-2-1-base')
+    parser.add_argument('--orig_image_path', default="/home/dongli911/.wan/Project/AIGC/stablediffusion/outputs/txt2img-samples/samples/00081.png")
+    parser.add_argument('--key_hex', default="5822ff9cce6772f714192f43863f6bad1bf54b78326973897e6b66c3186b77a7")
+    # nonce_hex=14192f43863f6bad1bf54b7832697389
+    parser.add_argument('--nonce_hex', default="")
+    parser.add_argument('--original_message_hex', default="6c746865726f6c746865726f6c746865726f6c746865726f6c746865726f6c74")
+    parser.add_argument('--num_inference_steps', default=100)
+    parser.add_argument('--l', default=1, help="the size of slide windows for m")
+    args = parser.parse_args()
+
+    # 假设reversed_latents是从图像中提取的逆向噪声矩阵
+    reversed_latents = exactract_latents(args)
+
+    # 将十六进制字符串转换为字节串
+    key = bytes.fromhex(args.key_hex)
+    # 
+    if args.nonce_hex!="":
+        # 使用参数的nonce_hex
+        nonce = bytes.fromhex(args.nonce_hex)
+    else:
+        # 使用固定的nonce
+        nonce_hex = args.key_hex[16:48]
+        # 将nonce_hex转换为字节
+        nonce = bytes.fromhex(nonce_hex)
+    
+    # 恢复原始消息
+    exactracted_message = recover_exactracted_message(reversed_latents, key, nonce, args.l)
+    print("Recovered message :\n", exactracted_message)
+
+    # 将十六进制字符串转换为二进制字符串表示
+    original_message = args.original_message_hex
+    binary_original_message = bin(int(original_message, 16))[2:].zfill(len(original_message) * 4)  # 每个十六进制数转换为4位二进制数
+
+    print("Original message:\n",binary_original_message)
+
+    # 确保 binary_string 和 exactracted_message 的长度相同
+    binary_original_message = binary_original_message[:len(exactracted_message)]
+
+    # 计算位准确率
+    matching_bits = sum(b1 == b2 for b1, b2 in zip(binary_original_message, exactracted_message))
+    accuracy = matching_bits / len(exactracted_message)
+
+    print("Bit accuracy: ",accuracy)
+    
---
+++ run_tree_ring_watermark_gs.py
@@ -0,0 +1,274 @@
+import argparse
+import wandb
+import copy
+from tqdm import tqdm
+from statistics import mean, stdev
+from sklearn import metrics
+
+import torch
+
+from inverse_stable_diffusion_gs import InversableStableDiffusionPipeline
+from diffusers import DPMSolverMultistepScheduler
+import open_clip
+from optim_utils import *
+from io_utils import *
+def init_gs_Z_s_T():
+    import numpy as np
+    from scipy.stats import norm
+    from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+    from cryptography.hazmat.backends import default_backend
+    import os
+
+    # 步骤1：生成256bit的随机水印消息k
+    k = os.urandom(32)  # 32 bytes * 8 bits/byte = 256 bits
+    print("=============k===============")
+    print(k)
+    print("=============k===============")
+    # 步骤2：扩散过程，复制64份
+    s_d = k * 64
+
+    # 步骤3：使用ChaCha20加密
+    key = os.urandom(32)  # 生成一个256bit的随机密钥
+    nonce = os.urandom(16)  # 生成一个随机nonce
+    cipher = Cipher(algorithms.ChaCha20(key, nonce), mode=None, backend=default_backend())
+    encryptor = cipher.encryptor()
+    m = encryptor.update(s_d) + encryptor.finalize()
+
+    # 将m转换为二进制形式
+    m_bits = ''.join(format(byte, '08b') for byte in m)
+
+    # 初始化结果列表，用于存储每个窗口的处理结果
+    results = []
+
+    # 窗口大小l，可以是除了1以外的其他值
+    l = 1  # 例如，改变这里为需要的窗口大小
+
+    index=0
+    Z_s_T_array = np.zeros((4, 64, 64))
+    # 遍历m的二进制表示，根据窗口大小l进行切割
+    for i in range(0, len(m_bits), l):
+        window = m_bits[i:i+l]
+        y = int(window, 2)  # 将窗口内的二进制序列转换为整数y
+
+        # 生成随机u
+        u = np.random.uniform(0, 1)
+
+        # 计算z^s_T
+        z_s_T = norm.ppf((u + y) / 2**l)
+        Z_s_T_array[index // (64*64), (index // 64) % 64, index % 64] = z_s_T
+        index+=1
+        # 提取i
+        i_extracted = np.floor(2**l * norm.cdf(z_s_T))
+
+        # 存储结果
+        results.append((y, i_extracted, z_s_T))
+
+    # print(len(Z_s_T_array),len(Z_s_T_array[0]),len(Z_s_T_array[0][0]),Z_s_T_array[0][0][0])
+    return Z_s_T_array
+
+def main(args):
+    table = None
+    if args.with_tracking:
+        wandb.init(project='diffusion_watermark', name=args.run_name, tags=['tree_ring_watermark'])
+        wandb.config.update(args)
+        table = wandb.Table(columns=['gen_no_w', 'no_w_clip_score', 'gen_w', 'w_clip_score', 'prompt', 'no_w_metric', 'w_metric'])
+    
+    # load diffusion model
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    
+    scheduler = DPMSolverMultistepScheduler.from_pretrained(args.model_id, subfolder='scheduler')
+    pipe = InversableStableDiffusionPipeline.from_pretrained(
+        args.model_id,
+        scheduler=scheduler,
+        torch_dtype=torch.float16,
+        revision='fp16',
+        )
+    pipe = pipe.to(device)
+
+    # reference model
+    if args.reference_model is not None:
+        ref_model, _, ref_clip_preprocess = open_clip.create_model_and_transforms(args.reference_model, pretrained=args.reference_model_pretrain, device=device)
+        ref_tokenizer = open_clip.get_tokenizer(args.reference_model)
+
+    # dataset
+    dataset, prompt_key = get_dataset(args)
+
+    tester_prompt = '' # assume at the detection time, the original prompt is unknown
+    text_embeddings = pipe.get_text_embedding(tester_prompt)
+    
+
+    # ground-truth patch
+    gt_patch = get_watermarking_pattern(pipe, args, device)
+
+    results = []
+    clip_scores = []
+    clip_scores_w = []
+    no_w_metrics = []
+    w_metrics = []
+
+    for i in tqdm(range(args.start, args.end)):
+        seed = i + args.gen_seed
+        
+        current_prompt = dataset[i][prompt_key]
+        
+        ### generation
+        # generation without watermarking
+        set_random_seed(seed)
+        init_latents_no_w = pipe.get_random_latents()
+        outputs_no_w = pipe(
+            current_prompt,
+            num_images_per_prompt=args.num_images,
+            guidance_scale=args.guidance_scale,
+            num_inference_steps=args.num_inference_steps,
+            height=args.image_length,
+            width=args.image_length,
+            latents=init_latents_no_w,
+            )
+        orig_image_no_w = outputs_no_w.images[0]
+        
+        # generation with watermarking
+        if init_latents_no_w is None:
+            set_random_seed(seed)
+            init_latents_w = pipe.get_random_latents()
+        else:
+            init_latents_w = copy.deepcopy(init_latents_no_w)
+
+        # get watermarking mask
+        watermarking_mask = get_watermarking_mask(init_latents_w, args, device)
+
+        # inject watermark
+        # init_latents_w = inject_watermark(init_latents_w, watermarking_mask, gt_patch, args)
+        Z_s_T_arrays = [init_gs_Z_s_T() for _ in range(1)]
+
+        # 将numpy数组转换为torch张量，并堆叠它们形成start_code
+        init_latents_w = torch.stack([torch.tensor(Z_s_T_array).float() for Z_s_T_array in Z_s_T_arrays]).to(device)
+
+        outputs_w = pipe(
+            current_prompt,
+            num_images_per_prompt=args.num_images,
+            guidance_scale=args.guidance_scale,
+            num_inference_steps=args.num_inference_steps,
+            height=args.image_length,
+            width=args.image_length,
+            latents=init_latents_w,
+            )
+        orig_image_w = outputs_w.images[0]
+
+        ### test watermark
+        # distortion
+        orig_image_no_w_auged, orig_image_w_auged = image_distortion(orig_image_no_w, orig_image_w, seed, args)
+
+        # reverse img without watermarking
+        img_no_w = transform_img(orig_image_no_w_auged).unsqueeze(0).to(text_embeddings.dtype).to(device)
+        image_latents_no_w = pipe.get_image_latents(img_no_w, sample=False)
+
+        reversed_latents_no_w = pipe.forward_diffusion(
+            latents=image_latents_no_w,
+            text_embeddings=text_embeddings,
+            guidance_scale=1,
+            num_inference_steps=args.test_num_inference_steps,
+        )
+
+        # reverse img with watermarking
+        img_w = transform_img(orig_image_w_auged).unsqueeze(0).to(text_embeddings.dtype).to(device)
+        image_latents_w = pipe.get_image_latents(img_w, sample=False)
+
+        reversed_latents_w = pipe.forward_diffusion(
+            latents=image_latents_w,
+            text_embeddings=text_embeddings,
+            guidance_scale=1,
+            num_inference_steps=args.test_num_inference_steps,
+        )
+
+        # eval
+        no_w_metric, w_metric = eval_watermark(reversed_latents_no_w, reversed_latents_w, watermarking_mask, gt_patch, args)
+
+        if args.reference_model is not None:
+            sims = measure_similarity([orig_image_no_w, orig_image_w], current_prompt, ref_model, ref_clip_preprocess, ref_tokenizer, device)
+            w_no_sim = sims[0].item()
+            w_sim = sims[1].item()
+        else:
+            w_no_sim = 0
+            w_sim = 0
+
+        results.append({
+            'no_w_metric': no_w_metric, 'w_metric': w_metric, 'w_no_sim': w_no_sim, 'w_sim': w_sim,
+        })
+
+        no_w_metrics.append(-no_w_metric)
+        w_metrics.append(-w_metric)
+
+        if args.with_tracking:
+            if (args.reference_model is not None) and (i < args.max_num_log_image):
+                # log images when we use reference_model
+                table.add_data(wandb.Image(orig_image_no_w), w_no_sim, wandb.Image(orig_image_w), w_sim, current_prompt, no_w_metric, w_metric)
+            else:
+                table.add_data(None, w_no_sim, None, w_sim, current_prompt, no_w_metric, w_metric)
+
+            clip_scores.append(w_no_sim)
+            clip_scores_w.append(w_sim)
+
+    # roc
+    preds = no_w_metrics +  w_metrics
+    t_labels = [0] * len(no_w_metrics) + [1] * len(w_metrics)
+
+    fpr, tpr, thresholds = metrics.roc_curve(t_labels, preds, pos_label=1)
+    auc = metrics.auc(fpr, tpr)
+    acc = np.max(1 - (fpr + (1 - tpr))/2)
+    low = tpr[np.where(fpr<.01)[0][-1]]
+
+    if args.with_tracking:
+        wandb.log({'Table': table})
+        wandb.log({'clip_score_mean': mean(clip_scores), 'clip_score_std': stdev(clip_scores),
+                   'w_clip_score_mean': mean(clip_scores_w), 'w_clip_score_std': stdev(clip_scores_w),
+                   'auc': auc, 'acc':acc, 'TPR@1%FPR': low})
+    
+    print(f'clip_score_mean: {mean(clip_scores)}')
+    print(f'w_clip_score_mean: {mean(clip_scores_w)}')
+    print(f'auc: {auc}, acc: {acc}, TPR@1%FPR: {low}')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='diffusion watermark')
+    parser.add_argument('--run_name', default='test')
+    parser.add_argument('--dataset', default='Gustavosta/Stable-Diffusion-Prompts')
+    parser.add_argument('--start', default=0, type=int)
+    parser.add_argument('--end', default=10, type=int)
+    parser.add_argument('--image_length', default=512, type=int)
+    parser.add_argument('--model_id', default='stabilityai/stable-diffusion-2-1-base')
+    parser.add_argument('--with_tracking', action='store_true')
+    parser.add_argument('--num_images', default=1, type=int)
+    parser.add_argument('--guidance_scale', default=7.5, type=float)
+    parser.add_argument('--num_inference_steps', default=50, type=int)
+    parser.add_argument('--test_num_inference_steps', default=None, type=int)
+    parser.add_argument('--reference_model', default=None)
+    parser.add_argument('--reference_model_pretrain', default=None)
+    parser.add_argument('--max_num_log_image', default=100, type=int)
+    parser.add_argument('--gen_seed', default=0, type=int)
+
+    # watermark
+    parser.add_argument('--w_seed', default=999999, type=int)
+    parser.add_argument('--w_channel', default=0, type=int)
+    parser.add_argument('--w_pattern', default='rand')
+    parser.add_argument('--w_mask_shape', default='circle')
+    parser.add_argument('--w_radius', default=10, type=int)
+    parser.add_argument('--w_measurement', default='l1_complex')
+    parser.add_argument('--w_injection', default='complex')
+    parser.add_argument('--w_pattern_const', default=0, type=float)
+    
+    # for image distortion
+    parser.add_argument('--r_degree', default=None, type=float)
+    parser.add_argument('--jpeg_ratio', default=None, type=int)
+    parser.add_argument('--crop_scale', default=None, type=float)
+    parser.add_argument('--crop_ratio', default=None, type=float)
+    parser.add_argument('--gaussian_blur_r', default=None, type=int)
+    parser.add_argument('--gaussian_std', default=None, type=float)
+    parser.add_argument('--brightness_factor', default=None, type=float)
+    parser.add_argument('--rand_aug', default=0, type=int)
+
+    args = parser.parse_args()
+
+    if args.test_num_inference_steps is None:
+        args.test_num_inference_steps = args.num_inference_steps
+    
+    main(args)
