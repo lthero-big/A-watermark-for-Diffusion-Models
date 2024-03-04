24d6491bdbd8e14f3417089c712e0da7ab6aa0e2
lthero-big <ltherowlh@gmail.com>
2024-03-04 21:00:23 +0800

    add batch/single

--- extract.py
+++ extract.py
@@ -96,32 +96,40 @@ def get_result_for_one_image(args):
     extracted_message_bin = recover_exactracted_message(reversed_latents, args.key, args.nonce, args.l)
     # 
     original_message_bin, bit_accuracy = calculate_bit_accuracy(args.original_message_hex, extracted_message_bin)
+    # print(f"{os.path.basename(args.orig_image_path)}: Original Message: {original_message_bin} \n Extracted Message: {extracted_message_bin}\n Bit Accuracy: {bit_accuracy}\n")
+    print(f"{os.path.basename(args.orig_image_path)}, Bit Accuracy,{bit_accuracy}\n")
     return original_message_bin,extracted_message_bin,bit_accuracy
 
 def process_directory(args):
     # Get all image files in the directory
     image_files = glob.glob(os.path.join(args.directory, "*.png")) + glob.glob(os.path.join(args.directory, "*.jpg"))
 
-    with open("result.txt", "a") as result_file:
+    with open(args.directory+"/"+"result.txt", "a") as result_file:
+        result_file.write("========================================Batch Info==========================================================\n")
+        result_file.write(f"key_hex:{args.key_hex} \nnonce_hex:{args.nonce_hex} \noriginal_message_hex:{args.original_message_hex} \nnum_inference_steps:{args.num_inference_steps}\n")
+        result_file.write("========================================Batch Start==========================================================\n")
         for image_path in tqdm(image_files):
             args.orig_image_path = image_path
             try:
                 # Process each image
                 result=get_result_for_one_image(args)
-                result_file.write(f"{os.path.basename(image_path)}: Original Message: {result[0]} , Extracted Message: {result[1]}, Bit Accuracy: {result[2]}\n")
+                result_file.write(f"{os.path.basename(image_path)}, Bit Accuracy, {result[2]}\n")
+                # result_file.write(f"{os.path.basename(image_path)}: Original Message: {result[0]} , Extracted Message: {result[1]}, Bit Accuracy: {result[2]}\n")
             except Exception as e:
                 result_file.write(f"Error processing {image_path}: {e}\n")
+        result_file.write("========================================Batch End==========================================================\n\n")
     
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Extract watermark from a image')
     parser.add_argument('--model_id', default='stabilityai/stable-diffusion-2-1-base')
-    parser.add_argument('--directory', help='Directory containing images to process', default="/home/dongli911/.wan/Project/AIGC/stablediffusion/outputs/txt2img-samples/need2test")
-    parser.add_argument('--orig_image_path', default="/home/dongli911/.wan/Project/AIGC/stablediffusion/outputs/txt2img-samples/samples/00381.png")
-    parser.add_argument('--key_hex', required=True, help='Hexadecimal key used for encryption', default="5822ff9cce6772f714192f43863f6bad1bf54b78326973897e6b66c3186b77a7")
+    parser.add_argument('--directory', help='Directory containing images to process', default="/home/dongli911/.wan/Project/AIGC/stablediffusion/outputs/txt2img-samples/n2t")
+    parser.add_argument('--orig_image_path', default="/home/dongli911/.wan/Project/AIGC/stablediffusion/outputs/txt2img-samples/samples/00595_v2_1.png")
+    # , required=True
+    parser.add_argument('--key_hex', help='Hexadecimal key used for encryption', default="5822ff9cce6772f714192f43863f6bad1bf54b78326973897e6b66c3186b77a7")
     # nonce_hex=14192f43863f6bad1bf54b7832697389
-    parser.add_argument('--nonce_hex', default="", help='Hexadecimal nonce used for encryption, if empty will use part of the key')
-    parser.add_argument('--original_message_hex', default="6c746865726f6c746865726f6c746865726f6c746865726f6c746865726f6c74", help='Hexadecimal representation of the original message for accuracy calculation')
+    parser.add_argument('--nonce_hex', default="05072fd1c2265f6f2e2a4080a2bfbdd8", help='Hexadecimal nonce used for encryption, if empty will use part of the key')
+    parser.add_argument('--original_message_hex', default="6c746865726f0000000000000000000000000000000000000000000000000000", help='Hexadecimal representation of the original message for accuracy calculation')
     parser.add_argument('--num_inference_steps', default=100, type=int, help='Number of inference steps for the model')
     parser.add_argument('--l', default=1, type=int, help="The size of slide windows for m")
     args = parser.parse_args()
@@ -130,10 +138,10 @@ if __name__ == "__main__":
     args.key = bytes.fromhex(args.key_hex)
     if args.nonce_hex!="":
         # 使用参数的nonce_hex
-        nonce = bytes.fromhex(args.nonce_hex)
+        args.nonce = bytes.fromhex(args.nonce_hex)
     else:
         # 使用固定的nonce, 将nonce_hex转换为字节
-        nonce = bytes.fromhex(args.key_hex[16:48])
+        args.nonce = bytes.fromhex(args.key_hex[16:48])
     
     # 批处理
     process_directory(args)
@@ -142,26 +150,4 @@ if __name__ == "__main__":
     # get_result_for_one_image(args)
 
 
-
-    # # 假设reversed_latents是从图像中提取的逆向噪声矩阵
-    # reversed_latents = exactract_latents(args)
-
-    # # 恢复原始消息
-    # exactracted_message = recover_exactracted_message(reversed_latents, key, nonce, args.l)
-    # print("Recovered message :\n", exactracted_message)
-
-    # # 将十六进制字符串转换为二进制字符串表示
-    # original_message = args.original_message_hex
-    # binary_original_message = bin(int(original_message, 16))[2:].zfill(len(original_message) * 4)  # 每个十六进制数转换为4位二进制数
-
-    # print("Original message:\n",binary_original_message)
-
-    # # 确保 binary_string 和 exactracted_message 的长度相同
-    # binary_original_message = binary_original_message[:len(exactracted_message)]
-
-    # # 计算位准确率
-    # matching_bits = sum(b1 == b2 for b1, b2 in zip(binary_original_message, exactracted_message))
-    # accuracy = matching_bits / len(exactracted_message)
-
-    # print("Bit accuracy: ",accuracy)
     
