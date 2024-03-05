5732bb645251d0eb15fcac6890d2af8f7e7982e3
lthero-big <ltherowlh@gmail.com>
2024-03-05 21:31:52 +0800

    add DDIM inversion solver to inverse the image

--- extract.py
+++ extract.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 from statistics import mean, stdev
 from sklearn import metrics
 from scipy.stats import norm
-from diffusers import DPMSolverMultistepScheduler
+from diffusers import DPMSolverMultistepScheduler,StableDiffusionPipeline, DDIMInverseScheduler, AutoencoderKL, DDIMScheduler
 import open_clip
 from optim_utils import *
 from io_utils import *
@@ -15,21 +15,71 @@ from cryptography.hazmat.backends import default_backend
 import numpy as np
 from scipy.stats import norm
 
+from typing import Union, Tuple, Optional
+from torchvision import transforms as tvt
+
+
+# Used for exactract_latents_DDIM_inversion
+def load_image(imgname: str, target_size: Optional[Union[int, Tuple[int, int]]] = None) -> torch.Tensor:
+    pil_img = Image.open(imgname).convert('RGB')
+    if target_size is not None:
+        if isinstance(target_size, int):
+            target_size = (target_size, target_size)
+        pil_img = pil_img.resize(target_size, Image.Resampling.LANCZOS)
+    return tvt.ToTensor()(pil_img)[None, ...]  # add batch dimension
+
+# Used for exactract_latents_DDIM_inversion
+def img_to_latents(x: torch.Tensor, vae: AutoencoderKL):
+    x = 2. * x - 1.
+    posterior = vae.encode(x).latent_dist
+    latents = posterior.mean * 0.18215
+    return latents
+
+
+@torch.no_grad()
+def  exactract_latents_DDIM_inversion(args) -> torch.Tensor:
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    dtype = torch.float16
+
+    inverse_scheduler = DDIMInverseScheduler.from_pretrained(args.model_id, subfolder='scheduler')
+    pipe = StableDiffusionPipeline.from_pretrained(args.model_id,
+                                                   scheduler=inverse_scheduler,
+                                                   safety_checker=None,
+                                                   torch_dtype=dtype)
+    pipe.to(device)
+    vae = pipe.vae
+
+    input_img = load_image(args.single_image_path).to(device=device, dtype=dtype)
+    latents = img_to_latents(input_img, vae)
+
+    inv_latents, _ = pipe(prompt="", negative_prompt="", guidance_scale=1.,
+                          width=input_img.shape[-1], height=input_img.shape[-2],
+                          output_type='latent', return_dict=False,
+                          num_inference_steps=args.num_inference_steps, latents=latents)
+    return inv_latents.cpu()
+
+
 def exactract_latents(args):
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    scheduler = DPMSolverMultistepScheduler.from_pretrained(args.model_id, subfolder='scheduler')
+    # 
+    if args.scheduler=="DPMs":
+        scheduler = DPMSolverMultistepScheduler.from_pretrained(args.model_id, subfolder='scheduler')
+    elif args.scheduler=="DDIM":
+        scheduler = DDIMInverseScheduler.from_pretrained(args.model_id, subfolder='scheduler')
+    
     pipe = InversableStableDiffusionPipeline.from_pretrained(
         args.model_id,
         scheduler=scheduler,
         torch_dtype=torch.float16,
         revision='fp16',
+        safety_checker=None,
         )
     pipe = pipe.to(device)
     tester_prompt = ''
     # assume at the detection time, the original prompt is unknown，原论文中使用的就是空prompt
     text_embeddings = pipe.get_text_embedding(tester_prompt)
     # 格式PIL.Image.Image
-    orig_image = Image.open(args.single_image_path)
+    orig_image = Image.open(args.single_image_path).convert("RGB")
     # 
     img_w = transform_img(orig_image).unsqueeze(0).to(torch.float16).to(device)
     # 
@@ -69,7 +119,7 @@ def recover_exactracted_message(reversed_latents, key, nonce, l=1):
 
     # 将重构的k转换为十六进制表示
     k_reconstructed_hex = k_reconstructed.hex()
-    
+
     # 将重构的k转换为二进制表示
     k_reconstructed_bin = ''.join(format(byte, '08b') for byte in k_reconstructed)
     
@@ -116,23 +166,29 @@ def process_directory(args):
                 result_file.write(f"{os.path.basename(image_path)}, Bit Accuracy, {result[2]}\n")
                 # result_file.write(f"{os.path.basename(image_path)}: Original Message: {result[0]} , Extracted Message: {result[1]}, Bit Accuracy: {result[2]}\n")
             except Exception as e:
+                print(f"Error processing {image_path}: {e}\n")
                 result_file.write(f"Error processing {image_path}: {e}\n")
         result_file.write("========================================Batch End==========================================================\n\n")
     
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Extract watermark from a image')
+    # 模型选择
+    # stabilityai/stable-diffusion-2-1
+    # stabilityai/stable-diffusion-2-1-base
     parser.add_argument('--model_id', default='stabilityai/stable-diffusion-2-1-base')
     # /home/dongli911/.wan/Project/AIGC/stablediffusion/outputs/txt2img-samples/n2t
     parser.add_argument('--image_directory_path', default="/home/dongli911/.wan/Project/AIGC/stablediffusion/outputs/txt2img-samples/n2t", help='The path of directory containing images to process')
-    # /home/dongli911/.wan/Project/AIGC/stablediffusion/outputs/txt2img-samples/samples/00595_v2_1.png
+    # /home/dongli911/.wan/Project/AIGC/stablediffusion/outputs/txt2img-samples/need2test/v2-1_512_00642-1_DDIM_50s.png
     parser.add_argument('--single_image_path', default="")
     # , required=True
     parser.add_argument('--key_hex', default="5822ff9cce6772f714192f43863f6bad1bf54b78326973897e6b66c3186b77a7", help='Hexadecimal key used for encryption')
     # nonce_hex=14192f43863f6bad1bf54b7832697389
     parser.add_argument('--nonce_hex', default="05072fd1c2265f6f2e2a4080a2bfbdd8", help='Hexadecimal nonce used for encryption, if empty will use part of the key')
     parser.add_argument('--original_message_hex', default="6c746865726f0000000000000000000000000000000000000000000000000000", help='Hexadecimal representation of the original message for accuracy calculation')
-    parser.add_argument('--num_inference_steps', default=100, type=int, help='Number of inference steps for the model')
+    parser.add_argument('--num_inference_steps', default=50, type=int, help='Number of inference steps for the model')
+    parser.add_argument('--scheduler', default="DPMs", help="Choose a scheduler between 'DPMs' and 'DDIM' to inverse the image")
+    
     parser.add_argument('--l', default=1, type=int, help="The size of slide windows for m")
     args = parser.parse_args()
 
