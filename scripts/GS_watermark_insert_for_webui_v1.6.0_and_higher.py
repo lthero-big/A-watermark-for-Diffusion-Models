8f7d5532bbc923bdfc4744c354c87f995116bede
lthero <70460095+lthero-big@users.noreply.github.com>
2024-07-17 08:52:27 +0000

    Update GS_watermark_insert_for_webui_v1.6.0_and_higher.py

--- scripts/GS_watermark_insert_for_webui_v1.6.0_and_higher.py
+++ scripts/GS_watermark_insert_for_webui_v1.6.0_and_higher.py
@@ -10,81 +10,81 @@ import numpy as np
 from scipy.stats import norm
 import os
 from datetime import datetime
+import numpy as np
+import modules.rng as rng
+from modules.rng import ImageRNG
 
 global_message = ""
 global_key = ""
 global_nonce = ""
+global_use_treering = 0
+global_use_randomSeed = 0
+global_randomSeed=42
+global_use_repeat=0
 
 
 def init_gs_Z_s_T():
-    global global_message, global_key, global_nonce
-    print("===================init_gs_Z_s_T======================")
+    global global_message, global_key, global_nonce, global_use_treering,global_randomSeed,global_use_randomSeed,global_use_repeat
+
+    rng = np.random.RandomState(seed=global_randomSeed)  
+
+    if int(global_use_repeat)==1:
+        LengthOfMessage_bytes=8
+    else:
+        LengthOfMessage_bytes=32
+
     if global_message:
-        # 将消息转换为字节串
         message_bytes = str(global_message).encode()
-        # 确保编码后的消息为256位（32字节）
-        if len(message_bytes) < 32:
-            padded_message = message_bytes + b'\x00' * (32 - len(message_bytes))
+        if len(message_bytes) < LengthOfMessage_bytes:
+            padded_message = message_bytes + b'\x00' * (LengthOfMessage_bytes - len(message_bytes))
         else:
-            padded_message = message_bytes[:32]
+            padded_message = message_bytes[:LengthOfMessage_bytes]
         k = padded_message
     else:
-        # 如果message为空，生成256bit的随机水印消息k
-        k = os.urandom(32)
+        k = os.urandom(LengthOfMessage_bytes)
 
-    # 扩散过程，复制64份
+    if int(global_use_repeat)==1:
+        k=k*4
+
     s_d = k * 64
 
-    # 使用ChaCha20加密
-    # 默认使用传入参数的key_hex和nonce_hex
     if global_key != "" and global_nonce != "":
-        # 将十六进制字符串转换为字节串
         key = bytes.fromhex(global_key)
-        # 使用参数的nonce_hex
         nonce = bytes.fromhex(global_nonce)
-    # nonce_hex可以省略，使用key_hex的中心16字节
     elif global_key != "" and global_nonce == "":
-        # 将十六进制字符串转换为字节串
         key = bytes.fromhex(global_key)
-        # 使用固定的nonce
         nonce_hex = global_key[16:48]
-        # 将nonce_hex转换为字节
         nonce = bytes.fromhex(nonce_hex)
     else:
         key = os.urandom(32)
         nonce = os.urandom(16)
 
-    # 加密
     cipher = Cipher(algorithms.ChaCha20(key, nonce), mode=None, backend=default_backend())
     encryptor = cipher.encryptor()
     m = encryptor.update(s_d) + encryptor.finalize()
-    # 将m转换为二进制形式,m服从均匀分布
     m_bits = ''.join(format(byte, '08b') for byte in m)
 
-    # 窗口大小l，可以是除了1以外的其他值
-    l = 1  # 例如，改变这里为需要的窗口大小
+    l = 1  
 
     index = 0
     Z_s_T_array = np.zeros((4, 64, 64))
-    # 遍历m的二进制表示，根据窗口大小l进行切割
     for i in range(0, len(m_bits), l):
         window = m_bits[i:i + l]
-        y = int(window, 2)  # 将窗口内的二进制序列转换为整数y
-
-        # 生成随机u
-        u = np.random.uniform(0, 1)
-        # 计算z^s_T
+        y = int(window, 2)  
+        if global_use_randomSeed==0:
+            u = np.random.uniform(0, 1)
+        else:
+            u = rng.uniform(0, 1)
         z_s_T = norm.ppf((u + y) / 2 ** l)
         Z_s_T_array[index // (64 * 64), (index // 64) % 64, index % 64] = z_s_T
         index += 1
 
-    # 将数据写入文件
-    # Get the current time
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     with open(f'info_data.txt', 'a') as f:
         f.write(f"Time: {current_time}\n")
         f.write(f'key: {key.hex()}\n')
         f.write(f'nonce: {nonce.hex()}\n')
+        f.write(f'randomSeed: {global_randomSeed}\n')  
         f.write(f'message: {k.hex()}\n')
         f.write('----------------------\n')
 
@@ -93,7 +93,7 @@ def init_gs_Z_s_T():
 
 def advanced_creator(shape, seeds, subseeds=None, subseed_strength=0.0, seed_resize_from_h=0, seed_resize_from_w=0,
                      p=None):
-    global global_message, global_key, global_nonce
+    global global_message, global_key, global_nonce,global_use_treering,global_randomSeed,global_use_randomSeed,global_use_repeat
     noise = torch.tensor(init_gs_Z_s_T()).float().to(shared.device)
     noise_with_new_dim = noise.unsqueeze(0)
     return noise_with_new_dim
@@ -107,10 +107,6 @@ def create_generator(seed):
     return generator
 
 def randn_without_seed(shape, generator=None):
-    """Generate a tensor with random numbers from a normal distribution using the previously initialized genrator.
-
-    Use either randn() or manual_seed() to initialize the generator."""
-
     if shared.opts.randn_source == "NV":
         return torch.asarray((generator or nv_rng).randn(shape), device=devices.device)
 
@@ -133,7 +129,7 @@ class modified_ImageRNG:
         self.is_first = True
 
     def first(self):
-        global global_message, global_key, global_nonce
+        global global_message, global_key, global_nonce,global_use_treering,global_randomSeed,global_use_randomSeed,global_use_repeat
         noise = torch.tensor(init_gs_Z_s_T()).float().to(shared.device)
         noise_with_new_dim = noise.unsqueeze(0)
         return noise_with_new_dim
@@ -151,32 +147,44 @@ class modified_ImageRNG:
         return torch.stack(xs).to(shared.device)
 
 
-#for newer webui >=1.6.0
-import modules.rng as rng
-from modules.rng import ImageRNG
+def set_seed(seed=None):
+    if seed is None or seed == -1:
+        seed = np.random.randint(0, 2**32 - 1)
+    return seed
+
+
 class Script(scripts.Script):
     def title(self):
         return "GS_watermark_insert"
 
     def ui(self, is_img2img):
-        global global_message, global_key, global_nonce
-        key_input = gr.Textbox(label='Input Key Here',
-                               value="5822ff9cce6772f714192f43863f6bad1bf54b78326973897e6b66c3186b77a7")
+        global global_message, global_key, global_nonce,global_use_treering,global_randomSeed,global_use_randomSeed,global_use_repeat
+        key_input = gr.Textbox(label='Input Key Here',value="5822ff9cce6772f714192f43863f6bad1bf54b78326973897e6b66c3186b77a7")
         nonce_input = gr.Textbox(label='Input Nonce Here', value="05072fd1c2265f6f2e2a4080a2bfbdd8")
-        message_input = gr.Textbox(label='Input Message Here',
-                                   value="")
-        return [message_input, key_input, nonce_input]
+        message_input = gr.Textbox(label='Input Message Here',value="")
+        use_repeat=gr.Textbox(label='1 means repeat message four times, 0 means not;Keep the length of message as 64bit and repeat it four times for eatch line.This can improve bit accuracy greatly',value="0")
+        use_randomSeed_input = gr.Textbox(label='1 means use use_randomSeed, 0 means not',value="0")
+        with gr.Row():
+            seed_input = gr.Number(label="Seed",value="42")
+            seed_button = gr.Button("Generate Random Seed")
+        
+        seed_button.click(fn=set_seed, inputs=None, outputs=seed_input)
+        
+        return [message_input, key_input, nonce_input,seed_input,use_randomSeed_input,use_repeat]
 
-    def run(self, p, message, key, nonce):
-        print("===================run======================")
+    def run(self, p, message, key, nonce,seed,use_randomSeed,use_repeat):
         real_creator = rng.ImageRNG
+        print("===================run======================")
         try:
             rng.ImageRNG = modified_ImageRNG
-            global global_message, global_key, global_nonce
+            global global_message, global_key, global_nonce,global_randomSeed,global_use_randomSeed,global_use_repeat
             global_message = message
             global_key = key
             global_nonce = nonce
-            print(global_key, global_nonce,global_message)
+            global_randomSeed=int(set_seed(seed))
+            global_use_randomSeed=int(use_randomSeed)
+            global_use_repeat=int(use_repeat)
+            print(global_key, global_nonce,global_message,global_randomSeed,global_use_randomSeed,global_use_repeat)
             return process_images(p)
         finally:
             rng.ImageRNG = modified_ImageRNG
